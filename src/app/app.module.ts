424d614725e543b0911c08967973da4093836981
jurgen <ramirezjurgen@gmail.com>
2018-09-13 15:44:14 -0600

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,29 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 //RUTAS
 import { APP_ROUTES } from './app.routes';
 
+//Modulos
+import { PagesModule } from './pages/pages.module';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './share/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HeadearComponent } from './share/headear/headear.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
-
-
+import { RegisterComponent } from './login/register.component';3
 
 @NgModule({
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
+    
   ],
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    HeadearComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    Graficas1Component,
-    PagesComponent,
-    RegisterComponent,
+    RegisterComponent
 
   ],
  
